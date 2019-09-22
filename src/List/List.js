a14e7734e09143a586769e4f789850a7d05537e2
tydillon <harrell_t@icloud.com>
2019-09-22 12:34:45 -0400

    first commit

---
+++ src/List/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const List = () => {
+  return (
+    <React.Fragment>
+      <Text>Hello world!</Text>
+    </React.Fragment>
+  );
+};
+
+export default List;
