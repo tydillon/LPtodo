10ad8c701d759cf63c1cfc793cdbb09675062d6f
tydillon <harrell_t@icloud.com>
2019-09-23 08:24:47 -0400

    cleaning up

--- App.js
+++ App.js
@@ -1,80 +1,8 @@
-import React, {Fragment, useState} from 'react';
-import ToDo from './src/Schema/ToDo';
-import Add from './src/Add/Add';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React from 'react';
+import Main from './src/Main/Main';
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-
-  const addToDo = input => {
-    setTodos([...todos, {text: input, isCompleted: false}]);
-  };
-
-  const toggleComplete = index => {
-    const current = [...todos];
-    if (current[index].isCompleted) {
-      current[index].isCompleted = false;
-    } else {
-      current[index].isCompleted = true;
-    }
-    setTodos(current);
-  };
-
-  const remove = index => {
-    const current = [...todos];
-    current.splice(index, 1);
-    setTodos(current);
-  };
-
-  return (
-    <View>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <View style={styles.app}>
-            <View style={styles.todoList}>
-              <Text style={styles.title}>To-do list</Text>
-              {todos.map((todo, index) => (
-                <ToDo
-                  key={index}
-                  index={index}
-                  todo={todo}
-                  toggleComplete={toggleComplete}
-                  remove={remove}
-                />
-              ))}
-              <Add addToDo={addToDo} />
-            </View>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </View>
-  );
+  return <Main />;
 };
-
-const styles = StyleSheet.create({
-  app: {
-    padding: '10%',
-    paddingTop: '50%',
-  },
-  todoList: {
-    backgroundColor: '#1f2561',
-    borderRadius: 4,
-    padding: '5%',
-  },
-  title: {
-    fontSize: 40,
-    alignSelf: 'center',
-    paddingBottom: 5,
-    color: 'white',
-  },
-});
 
 export default App;
---
+++ src/Main/Main.js
@@ -0,0 +1,80 @@
+import React, {useState} from 'react';
+import ToDo from '../Todo/ToDo';
+import Add from '../Add/Add';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const Main = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addToDo = input => {
+    setTodos([...todos, {text: input, isCompleted: false}]);
+  };
+
+  const toggleComplete = index => {
+    const current = [...todos];
+    if (current[index].isCompleted) {
+      current[index].isCompleted = false;
+    } else {
+      current[index].isCompleted = true;
+    }
+    setTodos(current);
+  };
+
+  const remove = index => {
+    const current = [...todos];
+    current.splice(index, 1);
+    setTodos(current);
+  };
+
+  return (
+    <View>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          <View style={styles.app}>
+            <View style={styles.todoList}>
+              <Text style={styles.title}>To-do list</Text>
+              {todos.map((todo, index) => (
+                <ToDo
+                  key={index}
+                  index={index}
+                  todo={todo}
+                  toggleComplete={toggleComplete}
+                  remove={remove}
+                />
+              ))}
+              <Add addToDo={addToDo} />
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  app: {
+    padding: '10%',
+    paddingTop: '50%',
+  },
+  todoList: {
+    backgroundColor: '#1f2561',
+    borderRadius: 4,
+    padding: '5%',
+  },
+  title: {
+    fontSize: 40,
+    alignSelf: 'center',
+    paddingBottom: 5,
+    color: 'white',
+  },
+});
+
+export default Main;
